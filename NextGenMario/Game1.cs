db072440825bf4f42cddadc05a6dcbd5b8d7bb68
ash-cmd <andrew.hariyanto@gmail.com>
2023-04-06 16:53:05 -0400

    added packages

--- NextGenMario/Game1.cs
+++ NextGenMario/Game1.cs
@@ -14,6 +14,7 @@ public class Game1 : Game
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+        
     }
 
     protected override void Initialize()
