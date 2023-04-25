b28426694d942c58e4f0088799e8fbe1ab93cd4e
Andrew Hariyanto <51850304+andrewhariyanto@users.noreply.github.com>
2023-04-25 14:41:55 -0400

    rudimentary rock obstacle

--- NextGenMario/Game1.cs
+++ NextGenMario/Game1.cs
@@ -15,6 +15,7 @@ public class Game1 : Game
     Texture2D wallTexture;
     Texture2D wallTexture1;
     Texture2D bulletTexture;
+    Texture2D rockTexture;
 
     // Declare window size
     const int WINDOW_WIDTH = 1280;
@@ -30,8 +31,11 @@ public class Game1 : Game
     Player player;
 
     // Create reference for Wave
-    WaveHorizontal waveHorizontal;
+    //WaveHorizontal waveHorizontal;
     //WaveVertical waveVertical;
+
+    // Reference for Rock Manager
+    private RockManager rockManager;
 
     public Game1()
     {
@@ -61,6 +65,8 @@ public class Game1 : Game
         wallTexture = NewTexture(GraphicsDevice, 100, WINDOW_HEIGHT, Color.White);
         wallTexture1 = NewTexture(GraphicsDevice, WINDOW_WIDTH - 200, 100, Color.White);
 
+        rockTexture = Content.Load<Texture2D>("rock");
+
         // All bullets texture
         List<Texture2D> bulletTextureList = new List<Texture2D>(); 
         bulletTexture = NewTexture(GraphicsDevice, 25, 25, Color.White);
@@ -78,8 +84,11 @@ public class Game1 : Game
         };
 
         // Initialize the wave
-        waveHorizontal = new WaveHorizontal(new Vector2(WINDOW_WIDTH, 100), 300f, 0, wallTextureWave);
+        //waveHorizontal = new WaveHorizontal(new Vector2(WINDOW_WIDTH, 100), 300f, 0, wallTextureWave);
         //waveVertical = new WaveVertical(new Vector2(0, -1000), 200f, 0, wallTextureWave);
+
+        // Initialize rock
+        rockManager = new RockManager(new Vector2(WINDOW_WIDTH/2, WINDOW_HEIGHT/2), 500, rockTexture, 2);
 
         _environmentSprites = new List<Sprite>()
         {
@@ -116,9 +125,9 @@ public class Game1 : Game
         }
 
         // add all horizontal waves to environment list
-        foreach(Sprite wave in waveHorizontal.getWalls()){
+        /*foreach(Sprite wave in waveHorizontal.getWalls()){
             _environmentSprites.Add(wave);
-        }
+        }*/
 
         player._environmentSprites = _environmentSprites;
 
@@ -135,6 +144,7 @@ public class Game1 : Game
         _spriteBatch.Dispose();
         wallTexture.Dispose();
         wallTexture1.Dispose();
+        bulletTexture.Dispose();
     }
 
     protected override void Update(GameTime gameTime)
@@ -145,8 +155,10 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        waveHorizontal.Update(gameTime);
+        //waveHorizontal.Update(gameTime);
         //waveVertical.Update(gameTime);
+
+        rockManager.Update(gameTime);
 
         // Handle player update
         player.Update(gameTime);
@@ -174,13 +186,15 @@ public class Game1 : Game
             sprite.Draw(_spriteBatch);
         }
 
+        rockManager.Draw(_spriteBatch);
+
         // Draw the player
         player.Draw(_spriteBatch);
 
         _spriteBatch.DrawString(gameFont, "Player Health: "  + player.health.ToString(), new Vector2(0, 0), Color.Chocolate);
         _spriteBatch.DrawString(gameFont, "Timer: "  + timer.ToString("0.#"), new Vector2(WINDOW_WIDTH/2, 0), Color.Chocolate);
 
-        waveHorizontal.Draw(_spriteBatch);
+        //waveHorizontal.Draw(_spriteBatch);
         //waveVertical.Draw(_spriteBatch);
 
         _spriteBatch.End();
---
+++ NextGenMario/RockManager.cs
@@ -0,0 +1,168 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace NextGenMario;
+
+public class RockManager{
+
+    int BLOCKHEIGHT = 1400;
+
+    public Vector2 position;
+    public float speed;
+    public Wall[] walls;
+    private bool retract = false;
+    private int numberOfRetracts;
+    private Vector2 newPosition;
+
+    public Texture2D rockTexture;
+    public Vector2[] wallPositions = new Vector2[] {
+        new Vector2(60, -60),
+        new Vector2(60-1280, 60),
+        new Vector2(-60-1280, 60 - 720),
+        new Vector2(-60, -60-720),
+    };
+
+    public Vector2[] startPositions = new Vector2[] {
+        new Vector2(60, -60 + 300 + 120),
+        new Vector2(60-1280 - 120 - 580, 60),
+        new Vector2(-60-1280, 60 - 720 - 120 - 300),
+        new Vector2(-60 + 120 + 580, -60-720),
+    };
+
+    public RockManager(Vector2 position, float speed, Texture2D rockTexture, int numberOfRetracts){
+        this.position = position;
+        this.newPosition = position;
+        this.speed = speed;
+        this.rockTexture = rockTexture;
+        this.numberOfRetracts = numberOfRetracts;
+        initializeWalls();
+    }
+
+    private void initializeWalls(){
+        walls = new Wall[4];
+        for(int i = 0; i < walls.Length; i++){
+            walls[i] = (new Wall(rockTexture){
+                position = new Vector2(this.position.X + startPositions[i].X, this.position.Y + startPositions[i].Y),
+                speed = 0,
+            });
+        }
+    }
+
+    public Wall[] getWalls(){
+        return walls;
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        checkIfMax();
+        if(numberOfRetracts >= 0){
+            if(retract){
+                Retract(deltaTime);
+                if(this.position.X > this.newPosition.X){
+                    this.position.X -= Math.Abs(this.position.X - this.newPosition.X)/2 * deltaTime;
+                }
+                else {
+                    this.position.X += Math.Abs(this.position.X - this.newPosition.X)/2 * deltaTime;
+                }
+
+                if(this.position.Y > this.newPosition.Y){
+                    this.position.Y -= Math.Abs(this.position.Y - this.newPosition.Y)/2 * deltaTime;
+                }
+                else{
+                    this.position.Y += Math.Abs(this.position.Y - this.newPosition.Y)/2 * deltaTime;
+                }
+            }
+            else{
+                CloseIn(deltaTime);
+            }
+        }
+        //redrawWalls(deltaTime);
+    }
+
+    public void Draw(SpriteBatch spriteBatch){
+        foreach(Wall wall in walls){
+            spriteBatch.Draw(rockTexture, wall.position, null, Color.White, 0, new Vector2(0, 0), new Vector2(2, 2), SpriteEffects.None, 1);
+        }
+    }
+
+    private void CloseIn(float deltaTime){
+        for(int i = 0; i < walls.Length; i += 2){
+            walls[i].position.Y -= ((float) Math.Pow(-1, i/2)) * speed * deltaTime;
+        }
+
+        for(int i = 1; i < walls.Length; i += 2){
+            walls[i].position.X += ((float) Math.Pow(-1, i/2)) * speed * deltaTime;
+        }
+        /*
+        if(walls[0].position.Y > wallPositions[0].Y){
+            walls[0].position.Y -= speed * deltaTime;
+        }
+
+        if(walls[1].position.X < wallPositions[1].X){
+            walls[1].position.Y += speed * deltaTime;
+        }
+
+        if(walls[2].position.Y < wallPositions[2].Y){
+            walls[2].position.Y += speed * deltaTime;
+        }
+
+        if(walls[3].position.X > wallPositions[3].X){
+            walls[3].position.X -= speed * deltaTime;
+        }
+        */
+    }
+
+    private void Retract(float deltaTime){
+        for(int i = 0; i < walls.Length; i += 2){
+            walls[i].position.Y += ((float) Math.Pow(-1, i/2)) * speed * deltaTime;
+        }
+
+        for(int i = 1; i < walls.Length; i += 2){
+            walls[i].position.X -= ((float) Math.Pow(-1, i/2)) * speed * deltaTime;
+        }
+        /*
+        if(walls[0].position.Y < startPositions[0].Y){
+            walls[0].position.Y += speed * deltaTime;
+        }
+
+        if(walls[1].position.X > startPositions[1].X){
+            walls[1].position.Y -= speed * deltaTime;
+        }
+
+        if(walls[2].position.Y > startPositions[2].Y){
+            walls[2].position.Y -= speed * deltaTime;
+        }
+
+        if(walls[3].position.X < startPositions[3].X){
+            walls[3].position.X += speed * deltaTime;
+        }
+        */
+    }
+
+    private void checkIfMax(){
+        if(retract){
+            if(walls[0].position.Y > this.position.Y + startPositions[0].Y && walls[1].position.X < this.position.X + startPositions[1].X && walls[2].position.Y < this.position.Y + startPositions[2].Y && walls[3].position.X > this.position.X + startPositions[3].X){
+                retract = false;
+                numberOfRetracts--;
+            }
+        }
+        else{
+            if(walls[0].position.Y < this.position.Y + wallPositions[0].Y && walls[1].position.X > this.position.X + wallPositions[1].X && walls[2].position.Y > this.position.Y + wallPositions[2].Y && walls[3].position.X < this.position.X + wallPositions[3].X){
+                retract = true;
+                Random newRand = new Random();
+                int newNumX = newRand.Next() % 640 + 320;
+                int newNumY = newRand.Next() % 360 + 180;
+                newPosition = new Vector2(newNumX, newNumY);
+            }
+        }
+    }
+
+    private void redrawWalls(float deltaTime){
+        for(int i = 0; i < walls.Length; i++){
+           // walls[i].position.X = this.position.X + (i/2)*100;
+        }
+    }
+}
